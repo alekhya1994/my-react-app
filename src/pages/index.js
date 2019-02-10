920fcc6a1ae3d139b83de206dcc3fc6bce20008b
alekhya1994 <batchualekhya@gmail.com>
2019-02-10 14:42:07 +0100

    Design+ code react
    My react app

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="." keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Learn to design and code React apps</h1>
+    <p>Complete courses about the best tools and design systems.Prototype and build apps with React and swift.</p>
+    <p>Now go build something great.</p>
+    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div> */}
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
